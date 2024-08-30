a41a7d6bca65024da3582e3459ef14ab6d96024d
Thinh Duy <thinh.nd.60cntt@ntu.edu.vn>
2024-08-30 16:51:19 +0700

    config ci env

--- test/helpers/page.js
+++ test/helpers/page.js
@@ -4,7 +4,7 @@ const userFactory = require("../factorires/userFactory");
 
 class CustomPage {
   static async build() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
     const page = await browser.newPage();
     const customPage = new CustomPage(page, browser);
 
