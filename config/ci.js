bdeb51df6076870e55f43337022c3eaec133b37a
Thinh Duy <thinh.nd.60cntt@ntu.edu.vn>
2024-08-30 16:57:16 +0700

    fix config for ci env

--- config/ci.js
+++ config/ci.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID: "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI: "mongodb://127.0.0.1:27017/adv-node-db",
-  cookieKey: 123123123,
+  cookieKey: "123123123",
   redisUrl: 'redis://127.0.0.1:6379'
 };
